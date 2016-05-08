8375820469691c64dc9c02854f111260604bc012
Alex Banks <alex@moonhighway.com>
2016-05-07 20:14:54 -0700

    Adds a backend (node/express)

---
+++ app.js
@@ -0,0 +1,39 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var routes = require('./routes/index');
+var users = require('./routes/users');
+var app = express();
+// uncomment after placing your favicon in /dist
+//app.use(favicon(path.join(__dirname, 'dist', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'dist')));
+app.use('/api', routes);
+app.use('/users', users);
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.json({
+      message: err.message,
+      error: err
+    });
+  });
+}
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.json({
+    message: err.message
+  });
+});
+module.exports = app;
