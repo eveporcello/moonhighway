8f5ae8bc8076ea5a28d720d080b896e39541b7a2
Alex Banks <alex@moonhighway.com>
2016-05-07 20:18:43 -0700

    Lints code

--- app.js
+++ app.js
@@ -11,29 +11,29 @@ var app = express();
 //app.use(favicon(path.join(__dirname, 'dist', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'dist')));
 app.use('/api', routes);
 app.use('/users', users);
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+app.use(function (req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
+    app.use(function (err, req, res, next) {
+        res.status(err.status || 500);
+        res.json({
+            message: err.message,
+            error: err
+        });
+    });
+}
+app.use(function (err, req, res, next) {
     res.status(err.status || 500);
     res.json({
-      message: err.message,
-      error: err
+        message: err.message
     });
-  });
-}
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.json({
-    message: err.message
-  });
 });
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', function(req, res) {
-  res.json({ message: 'API - Under construction' });
+router.get('/', function (req, res) {
+    res.json({message: 'API - Under construction'});
 });
 
 module.exports = router;
