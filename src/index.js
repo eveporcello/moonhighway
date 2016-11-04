88682203e5e975d8236316ecda6cf927c1103bfc
Alex Banks <alex@moonhighway.com>
2016-11-03 18:07:39 -0700

    Step 1 : Simple Overlay

--- src/index.js
+++ src/index.js
@@ -3,8 +3,29 @@ import { render } from 'react-dom'
 import { Router, Route, Redirect, hashHistory } from 'react-router'
 import { HomePage } from './components/home'
 import { Whoops404 } from './components/ui'
+import { preload } from 'pic-loader'
+
+const removeOverlay = () => {
+    const overlay = document.getElementById("loading-overlay")
+    overlay.parentNode.removeChild(overlay)
+}
 
 window.React = React
+
+preload([
+    '/img/bgCD.png',
+    '/img/bgContact.png',
+    '/img/bgHome.png',
+    '/img/bgHTML.png',
+    '/img/bgNode.png',
+    '/img/bgReact.png',
+    '/img/cd.png',
+    '/img/html.png',
+    '/img/logo.png',
+    '/img/node.png',
+    '/img/react.png'
+]).then(removeOverlay)
+
 
 render(
     <Router history={hashHistory}>
@@ -15,9 +36,9 @@ render(
         <Route path="/continuous-delivery" component={HomePage}/>
         <Route path="/contact" component={HomePage}/>
         <Redirect from="/h5o-1" to="/"/>
-        <Redirect from="/h5o-2" to="/" />
-        <Redirect from="/h5o-3" to="/react" />
-        <Redirect from="/h5o-4" to="/node" />
+        <Redirect from="/h5o-2" to="/"/>
+        <Redirect from="/h5o-3" to="/react"/>
+        <Redirect from="/h5o-4" to="/node"/>
         <Redirect from="/h5o-5" to="/html-css"/>
         <Redirect from="/h5o-6" to="/continuous-delivery"/>
         <Redirect from="/h5o-7" to="/contact"/>
