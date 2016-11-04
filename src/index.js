08ce194bee6e117fa38d098c5ce7db6665bc2c7b
Alex Banks <alex@moonhighway.com>
2016-11-04 11:10:30 -0700

    Simplifies Loading Screen

--- src/index.js
+++ src/index.js
@@ -1,16 +1,12 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { Router, Route, Redirect, hashHistory } from 'react-router'
-import { HomePage } from './components/home'
-import { Whoops404 } from './components/ui'
 import { preload } from 'pic-loader'
+import routes from './routes'
 
-const removeOverlay = () => {
-    const overlay = document.getElementById("loading-overlay")
-    overlay.parentNode.removeChild(overlay)
-}
+const target = document.getElementById('react-container')
 
 window.React = React
+
 
 preload([
     '/img/bgCD.png',
@@ -24,25 +20,7 @@ preload([
     '/img/logo.png',
     '/img/node.png',
     '/img/react.png'
-]).then(removeOverlay)
-
-
-render(
-    <Router history={hashHistory}>
-        <Route path="/" component={HomePage}/>
-        <Route path="/react" component={HomePage}/>
-        <Route path="/node" component={HomePage}/>
-        <Route path="/html-css" component={HomePage}/>
-        <Route path="/continuous-delivery" component={HomePage}/>
-        <Route path="/contact" component={HomePage}/>
-        <Redirect from="/h5o-1" to="/"/>
-        <Redirect from="/h5o-2" to="/"/>
-        <Redirect from="/h5o-3" to="/react"/>
-        <Redirect from="/h5o-4" to="/node"/>
-        <Redirect from="/h5o-5" to="/html-css"/>
-        <Redirect from="/h5o-6" to="/continuous-delivery"/>
-        <Redirect from="/h5o-7" to="/contact"/>
-        <Route path="*" component={Whoops404}/>
-    </Router>,
-    document.getElementById('react-container')
-)
+]).then(() => {
+    target.className = 'fadeIn'
+    render(routes, target)
+})
---
+++ src/routes.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { Router, Route, Redirect, hashHistory } from 'react-router'
+import { HomePage } from './components/home'
+import { Whoops404 } from './components/ui'
+
+const routes = (
+    <Router history={hashHistory}>
+        <Route path="/" component={HomePage}/>
+        <Route path="/react" component={HomePage}/>
+        <Route path="/node" component={HomePage}/>
+        <Route path="/html-css" component={HomePage}/>
+        <Route path="/continuous-delivery" component={HomePage}/>
+        <Route path="/contact" component={HomePage}/>
+        <Redirect from="/h5o-1" to="/"/>
+        <Redirect from="/h5o-2" to="/"/>
+        <Redirect from="/h5o-3" to="/react"/>
+        <Redirect from="/h5o-4" to="/node"/>
+        <Redirect from="/h5o-5" to="/html-css"/>
+        <Redirect from="/h5o-6" to="/continuous-delivery"/>
+        <Redirect from="/h5o-7" to="/contact"/>
+        <Route path="*" component={Whoops404}/>
+    </Router>
+)
+
+export default routes
