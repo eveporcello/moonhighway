e17d754c2f9ae20356abd209ee2a85f531c67b91
Alex Banks <alex@moonhighway.com>
2016-11-06 12:45:09 -0800

    style and code for contact us

--- src/components/home/Contact.js
+++ src/components/home/Contact.js
@@ -1,6 +1,6 @@
-import TwitterIcon from 'react-icons/lib/fa/twitter'
-import FacebookIcon from 'react-icons/lib/fa/facebook'
-import GithubIcon from 'react-icons/lib/fa/github'
+import C from '../../config.json'
+import { SocialIcons, ExpandableSelectList } from '../ui'
+import '../../stylesheets/Contact.scss'
 
 export const expandBox = {
     '-30%': {
@@ -13,37 +13,54 @@ export const expandBox = {
     },
     '0%': {
         left: '5%',
-        height: '90%'
+        height: '63%'
     }
 }
 
-const Contact = ({index, screenScale}) =>
-    <section className="slide contact"
-             style={{
+const Contact = ({index, screenScale}) => {
+
+    let _email, _subjects, _message
+
+    const submit = () => {
+        alert(`
+TODO: Send Message
+to: ${_email.value}
+subjects: ${_subjects.value}
+message: ${_message.value}
+        `)
+    }
+
+    return (
+        <section className="slide contact"
+                 style={{
                  backgroundImage: `url(/img/bgContact.png)`,
                  zIndex: 1000-index
              }}
-        {...screenScale({ '0%': { top: '0px' }})}>
-        <div className="box" {...screenScale(expandBox)}>
-            <h1>Contact Us</h1>
-            <p>For migration and installation instructions, please get in touch.</p>
-            <p>
-                <a href="mailto:info@moonhighway.com">info@moonhighway.com</a>
-                <a href="tel:5305233084">530.523.3084</a>
-                <span>PO BOX 1578, Tahoe City, CA 96145</span>
-                <form action="javascript:void(0)">
-                    <fieldset>
-                        <p>Add Options</p>
-                    </fieldset>
-                    <input type="text"/>
-                    <textarea></textarea>
-                    <button>send</button>
+            {...screenScale({'0%': {top: '0px'}})}>
+            <div className="box" {...screenScale(expandBox)}>
+                <h1>Contact Us</h1>
+                <SocialIcons />
+                <p>For migration and installation instructions, please get in touch.</p>
+                <p>
+                    <a href="mailto:info@moonhighway.com">info@moonhighway.com</a>
+                    <span>|</span>
+                    <a href="tel:5305233084">530.523.3084</a>
+                    <span>PO BOX 1578, Tahoe City, CA 96145</span>
+                </p>
+                <form action="javascript:void(0)" onSubmit={submit}>
+                    <p>What are you interested in learning?</p>
+                    <ExpandableSelectList ref={i=> _subjects = i}
+                                          options={C.contact.subjects}>
+                        Choose all that apply
+                    </ExpandableSelectList>
+                    <input ref={i => _email = i} type="email" placeholder="email" required/>
+                    <textarea ref={i => _message = i} placeholder="Other thoughts or topics of interest?" />
+                    <button>SEND</button>
                 </form>
-                <TwitterIcon />
-                <FacebookIcon />
-                <GithubIcon />
-            </p>
-        </div>
-    </section>
+            </div>
+        </section>
+    )
+}
+
 
 export default Contact
--- src/components/ui/index.js
+++ src/components/ui/index.js
@@ -1,8 +1,12 @@
-import { PropTypes } from 'react'
+import { PropTypes, Component } from 'react'
 import MdEject from 'react-icons/lib/md/eject'
 import GithubIcon from 'react-icons/lib/go/mark-github'
 import TwitterIcon from 'react-icons/lib/fa/twitter'
 import FacebookIcon from 'react-icons/lib/fa/facebook'
+import DownTriangle from 'react-icons/lib/go/triangle-down'
+import Box from 'react-icons/lib/md/check-box-outline-blank'
+import Check from 'react-icons/lib/md/check-box'
+
 import '../../stylesheets/ui.scss'
 
 export const DownButton = ({onClick=f=>f}) =>
@@ -32,3 +36,83 @@ export const Whoops404 = () =>
     <div className="whoops-404">
         <h1>Whoops 404</h1>
     </div>
+
+export const SelectCheck = ({selected=false, select=f=>f, deselect=f=>f, children}) =>
+    <div className="select-check" onClick={(selected) ? deselect : select}>
+        {(selected) ? <Check /> : <Box />}
+        <span className={(selected) ? "selected" : ""}>
+            {children}
+        </span>
+    </div>
+
+export class ExpandableSelectList extends Component {
+
+    get value() {
+        return this.state.selectedOptions.join(',')
+    }
+
+    set value(val) {
+        if (Array.isArray(val)) {
+            const selectedOptions = val.split(',')
+            this.setState({selectedOptions})
+        }
+    }
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            expanded: false,
+            selectedOptions: []
+        }
+        this.toggle = this.toggle.bind(this)
+        this.add = this.add.bind(this)
+        this.remove = this.remove.bind(this)
+    }
+    toggle() {
+        const expanded = !this.state.expanded
+        this.setState({expanded})
+    }
+    add(option) {
+        if (!this.state.selectedOptions.some(sel => sel === option)) {
+            const selectedOptions = [
+                ...this.state.selectedOptions,
+                option
+            ]
+            this.setState({selectedOptions})
+        }
+    }
+    remove(option) {
+        const selectedOptions = this.state.selectedOptions.filter(opt => opt !== option)
+        this.setState({selectedOptions})
+    }
+    render() {
+        const { children, options } = this.props
+        const { expanded } = this.state
+        return (
+            <div className="expandable-select-list">
+                <button type="button"
+                        onClick={this.toggle}>{children} | <DownTriangle /></button>
+                {(expanded) ?
+                    <div className="options">
+                        {options.map((opt, i) =>
+                            <SelectCheck key={i}
+                                         selected={this.state.selectedOptions.some(sel=>opt===sel)}
+                                         select={() => this.add(opt)}
+                                         deselect={() => this.remove(opt)}>
+                                {opt}
+                            </SelectCheck>
+                        )}
+                    </div> : null
+                }
+            </div>
+        )
+    }
+}
+
+ExpandableSelectList.defaultProps = {
+    options: []
+}
+
+ExpandableSelectList.propTypes = {
+    options: PropTypes.array
+}
