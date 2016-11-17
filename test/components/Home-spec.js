ea45f936d31e5b604d8601681e15bc8b2e3aa42d
Alex Banks <alex@moonhighway.com>
2016-11-17 14:44:20 -0800

    Removes Rellax Test

--- test/components/Home-spec.js
+++ test/components/Home-spec.js
@@ -1,11 +1,9 @@
-import { expect } from 'chai'
-import { shallow } from 'enzyme'
-import { HomePage } from '../../src/components/home'
-
-describe("<HomePage /> Component", () => {
-
-    it("<HomePage /> renders div.relax-root", () =>
-        expect(shallow(<HomePage />).find('div.relax-root')).to.be.ok
-    )
-
-})
+//import { expect } from 'chai'
+//import { shallow } from 'enzyme'
+//import { HomePage } from '../../src/components/home'
+//
+//describe("<HomePage /> Component", () => {
+//
+//    it("<HomePage /> renders div.relax-root")
+//
+//})
