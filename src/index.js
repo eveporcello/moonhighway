124d0246401c43825048514a8a01b4a7eb6928dd
MoonTahoe <alex@moonhighway.com>
2017-04-26 15:23:11 -0700

    Adds Google Analytics Tracking

--- src/index.js
+++ src/index.js
@@ -10,22 +10,6 @@ const titles = C.preload.titles.map(t => '/img/titles/' + t)
 
 window.React = React
 
-// TODO: Test iPhone Emulators
-
-// TODO: Test iPad Emulators
-
-// TODO: Test Galaxy Emulator
-
-// TODO: Create Galaxy Pad Emulator
-
-// TODO: Test Galaxy Pad Emulator
-
-// TODO: Test Portrait Browsers
-
-// TODO: Test Landscape Browsers
-
-// TODO: Incorporate Google Analytics
-
 preload([...backgrounds, ...titles]).then(() => {
     setTimeout(() => render(routes, document.getElementById('react-container')), 200)
 })
