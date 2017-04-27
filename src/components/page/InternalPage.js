d77bc80e4672faa75c49dd8a35175609b4cab14b
MoonTahoe <alex@moonhighway.com>
2017-04-27 15:49:13 -0700

    CMS MVP loads markdown documents on /info/ route

--- src/components/page/InternalPage.js
+++ src/components/page/InternalPage.js
@@ -1,20 +1,53 @@
 import { Component } from 'react'
+import marked from 'marked'
 import '../../stylesheets/internal-page.scss'
+import Loading from 'react-loading-animation'
 
 export default class InternalPage extends Component {
+
   constructor(props) {
     super(props)
+    this.state = {
+      loaded: false,
+      content: null,
+      error: null
+    }
+  }
+
+  componentWillMount() {
+    document.body.style.backgroundColor = "white"
   }
 
   componentDidMount() {
-    document.body.style.backgroundColor = "white"
+    const { article } = this.props.params
+    fetch(`/content/${article}.md`)
+      .then(resp => resp.text())
+      .then(resp => resp.match(/<!DOCTYPE html>/) ?
+          `Whoops...\n==========\n\nWe are sorry, we cannot find content for __${article}__` :
+          resp
+      )
+      .then(markdown => marked(markdown))
+      .then(html => this.setState({
+        loaded: true,
+        content: {__html: html}
+      }))
+      .catch(error => this.setState({error}))
   }
 
   render() {
     const { params } = this.props
     return (
       <article className="internal-page">
-        <h1>Internal Page: {params.article}</h1>
+        {(this.state.loaded) ? (
+          <div className="contents" dangerouslySetInnerHTML={this.state.content} />
+        ) : (this.state.error) ? (
+          <div className="error">
+            <h1>Error loading content</h1>
+            <p>We've experienced an error trying to load: {params.article}</p>
+          </div>
+        ) : (
+          <Loading />
+        )}
       </article>
     )
   }
