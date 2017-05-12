ab9e887dca59c5f7e015f7df232d5bcf276f3fe6
MoonTahoe <alex@moonhighway.com>
2017-05-11 19:44:02 -0700

    Clean up Files - Improve Code ==================== * Removed class content files * Improved `routes.js` code...

--- src/routes.js
+++ src/routes.js
@@ -5,19 +5,25 @@ import { Whoops404 } from './components/ui'
 import InternalPage from './components/page/InternalPage'
 import ReactGA from 'react-ga'
 
-if (window.location.origin.match(/moonhighway.com/)) {
-    ReactGA.initialize('UA-40790507-1')
-}
+export const isProduction = (fTrue, fFalse) =>
+    (window.location.origin.match(/moonhighway.com/) && process.env.NODE_ENV === 'production' && !window.autoplaying) ?
+        (typeof fTrue === 'function') ? fTrue() : fTrue
+        : (typeof fFalse === 'function') ? fFalse() : fFalse
+
+export const isDevServer = (fTrue, fFalse) =>
+    (process.env.NODE_ENV === 'development' && window.location.origin.match(/http:\/\/localhost:3333/)) ?
+        (typeof fTrue === 'function') ? fTrue() : fTrue
+        : (typeof fFalse === 'function') ? fFalse() : fFalse
+
+isProduction(() => ReactGA.initialize('UA-40790507-1'))
 
 const routes = (
-    <Router history={(process.env.NODE_ENV === 'development' && window.location.origin.match(/http:\/\/localhost:3333/)) ? hashHistory : browserHistory} onUpdate={() => {
-        if (process.env.NODE_ENV === 'development' && window.location.origin.match(/http:\/\/localhost:3333/)) {
-            console.warn('using hashHistory for development')
-        }
-        if (window.location.origin.match(/moonhighway.com/) && !window.autoplaying) {
+    <Router history={isDevServer(hashHistory, browserHistory)} onUpdate={() => {
+        isDevServer(() => console.warn('using hashHistory for development'))
+        isProduction(() => {
             ReactGA.set({ page: window.location.pathname })
             ReactGA.pageview(window.location.pathname)
-        }
+        })
     }}>
         <Route path="/" component={HomePage}/>
         <Route path="/react" component={HomePage}/>
