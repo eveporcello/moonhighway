e4a75b3953f88373cfbf8580537cdd661657ca80
eveporcello <eve@moonhighway.com>
2017-07-05 15:25:27 -0700

    Fetches all lift data - just names

--- src/server/graphql/schema.js
+++ src/server/graphql/schema.js
@@ -1,9 +1,41 @@
-import { buildSchema } from 'graphql'
+import { GraphQLObjectType,
+         GraphQLString,
+         GraphQLList,
+         GraphQLSchema } from 'graphql'
+import fetch from 'node-fetch'
 
-var schema = buildSchema(`
-  type Query {
-    hello: String
-  }
-`)
+const snowtoothAPI = 'https://www.moonhighway.com/class/api/snowtooth'
 
-export default schema
+function snowtoothRequest(route) {
+    return fetch(`${snowtoothAPI}${route}`).then(res => res.json())
+}
+
+function fetchLifts() {
+    return snowtoothRequest('/lifts/')
+}
+
+const LiftType = new GraphQLObjectType({
+    name: 'Lift',
+    description: 'A ski lift',
+    fields: () => ({
+        name: {
+            type: GraphQLString,
+            resolve: lift => lift.name
+        }
+    })
+})
+
+const QueryType = new GraphQLObjectType({
+    name: 'Query',
+    description: 'Root query for the Snowtooth Graph',
+    fields: () => ({
+        allLifts: {
+            type: new GraphQLList(LiftType),
+            resolve: fetchLifts
+        }
+    })
+})
+
+export default new GraphQLSchema({
+    query: QueryType
+})
